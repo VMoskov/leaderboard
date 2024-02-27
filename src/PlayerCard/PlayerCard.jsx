96a2a052af32c525d81f5c97bb7693edda42b538
VMoskov <vedran.moskov@fer.hr>
2024-02-27 22:06:59 +0100

    restructured the file

--- src/PlayerCard/PlayerCard.jsx
+++ src/PlayerCard/PlayerCard.jsx
@@ -1,9 +1,9 @@
 export const PlayerCard = ({ player }) => {
     return (
-        <div className="player-card">
-            <img src={player.photo} alt="Player"/>
-            <h3>{player.name}</h3>
-            <p>Score: {player.score}</p>
+        <div className='player-card'>
+            <img src={ player.photo } alt='Player'/>
+            <h3>{ player.name }</h3>
+            <p>Score: { player.score }</p>
         </div>
     )
 }
