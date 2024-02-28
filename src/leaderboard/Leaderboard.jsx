6224b7ac72ec38ed9baaf7fad98d51ba71f6a9db
VMoskov <vedran.moskov@fer.hr>
2024-02-28 01:19:06 +0100

    loading data from backend

--- src/leaderboard/Leaderboard.jsx
+++ src/leaderboard/Leaderboard.jsx
@@ -1,16 +1,39 @@
 import { PlayerCard } from '../PlayerCard/PlayerCard';
+import { useState, useEffect } from 'react';
 
-export const Leaderboard = ({ players }) => {
+export const Leaderboard = () => {
+    const [scores, setScores] = useState({});
+    const playersFromLS = localStorage.getItem('players');
+    const initialPlayers = playersFromLS ? JSON.parse(playersFromLS) : {};
+    const [players, setPlayers] = useState(initialPlayers);
+    const [playerCardsKey, setPlayerCardsKey] = useState(0); // Initialize key state
+
+
+    const loadScores = () => {
+        fetch('http://localhost:8080/')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setScores(data);
+                for(let playerKey in data) {
+                    if(players[playerKey]) {
+                        players[playerKey].score = data[playerKey];
+                    }
+                }
+            })
+            .catch(error => console.error(error));
+    };
+
     return (
         <div className='leaderboard'>
             <div className='player-cards'>
                 {Object.keys(players).map((playerKey, index) => (
-                    <PlayerCard key={index} player={players[playerKey]}/>
+                    <PlayerCard key={`${playerKey}-${playerCardsKey}`} player={players[playerKey]} />
                 ))}
             </div>
             <div className='bottom-container'>
-                <button className='load-button'>Load</button>
+                <button onClick={loadScores} className='load-button'>Load</button>
             </div>
         </div>
     );
-}
+};
